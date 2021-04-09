8735581047a632e2025d0a0a1312f068a3e37249
Your Name <danielle.sisk@nyu.edu>
2021-04-08 20:12:26 -0400

    finished

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,13 @@ const fetch = require("node-fetch");
  * Returns a list of quizzes with titles and IDs
  */
 async function getQuizzes(req, res, next) {
-  res.json(quizzes)
+  let allQuizzesList = []; 
+  for(const key in quizzes){
+   let quizObject = {'title': quizzes[key]['title'], 'id': quizzes[key]['id']}
+     allQuizzesList.push(quizObject)
+
+  }
+  res.json(allQuizzesList)
 }
 
 /** 
@@ -16,7 +22,7 @@ async function getQuiz(req, res, next) {
   let subject = req.params.id; 
   
   if(!quizzes[subject]){
-    res.sendStatus(404)
+    res.send("No quiz found with that subject")
   } else{
     let quizObject = {"id":subject, "title": quizzes[subject]['title'], "questions":[]}; 
     let questionsArray = quizzes[subject]['questions'] // loop through the array and grab each object 
@@ -40,9 +46,12 @@ let correctCount = 0;
 let correctAnswerArray = []; //[ 'True', 'Dangling participle', 'their' ]
 let clientAnswerArray = []; 
 let subjectRequest = req.params.id; 
+if(!quizzes[subjectRequest]){
+  res.send("Please enter a valid quiz subject")
+  return; 
+}
  let questionsArray = quizzes[subjectRequest].questions // correct question/answer objects 
  let questionsObject = {}; 
- // only really need one array 
  let clientResponse = req.body['answers'] //{ question_1: '1', question_2: 'true' }  
 
  for(let i = 0; i < questionsArray.length; i++){
@@ -53,31 +62,27 @@ questionsObject[eachQuestionObject['id']] = eachQuestionObject['answer']
 let correctAnswer = eachQuestionObject['answer'] //(true, dangling participle, their) // also consider if array lengths are different 
 correctAnswerArray.push(correctAnswer)
  }
- 
+
  for(const key in clientResponse){
    let clientAnswer = clientResponse[key] // each client answer from 1 - n  (1, true, false); 
    clientAnswerArray.push(clientAnswer)
  }
 
 
-// loop through both arrays with a for loop and compare values at each i 
-
-// clientAnswerArray: [ '1', 'true', '1' ]
-// correctAnswerArray: [ 'True', 'Dangling participle', 'their' ]
-// check if lengths are equal and if not? 
-// console.log(clientAnswerArray[0] == correctAnswerArray[0])
-
+// loop through both arrays with a for loop and compare values at each iteration 
 
 if(clientAnswerArray.length == correctAnswerArray.length){ 
-for(let i = 0; i < clientAnswerArray.length; i++){
+for(let i = 0; i <= clientAnswerArray.length-1; i++){
+  // console.log(correctAnswerArray[i])
     if(clientAnswerArray[i].toLowerCase() == correctAnswerArray[i].toLowerCase()){ 
       correctCount++;       
     } else{
       incorrectCount++; 
     }
 }
-
-} 
+} else {
+  res.send("please correct the number of questions submitted")
+}
 // questions should be an object with question number as val answers as keys 
 if(correctCount + incorrectCount === correctAnswerArray.length){
  let reponseObject = 
@@ -91,36 +96,9 @@ if(!quizzes[subjectRequest]){
 } else {
   res.json(reponseObject)
 }
-} else{
-  res.send("please finish answering all of the questions")
-}
+} 
 
 }
-
-
-// update variables 
-// create object in send
-
-
-//   "answers": {
-//     "question_1": "1",
-//     "question_2": "true"
-//   }
-// }
-
-
-
-// Response
-// {
-//   "correct": 1, // return number of correc 
-//   "incorrect": 1,
-//   "questions": {
-//     "question_1": true,
-//     "question_2": false, <0- varianel that holds all correct answers 
-//   }
-// }
-  // TODO: Your code goes here
-
 
 module.exports = {
   getQuizzes,
